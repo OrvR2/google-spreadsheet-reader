65b54f7cc513622a6d4c9cb6544c80df1eb2108f
hdelnegro <hdelnegro@HDELNEGRO>
2016-11-16 11:58:38 -0300

    Adding comments

--- GoogleSpreadSheetReader/Program.cs
+++ GoogleSpreadSheetReader/Program.cs
@@ -3,6 +3,7 @@ using Google.Apis.Sheets.v4;
 using GoogleSpreadSheetReader.Model;
 using GoogleSpreadSheetReader.Provider;
 using Newtonsoft.Json;
+using System;
 using System.Configuration;
 using System.Xml;
 
@@ -12,24 +13,39 @@ namespace MySpreadsheetIntegration
     {
         static void Main(string[] args)
         {
-            var credential = ServiceCredentialsProvider.GetServiceCredentials();
+            Console.Title = "Google Reader";
 
-            var service = new SheetsService(new BaseClientService.Initializer()
+            try
             {
-                HttpClientInitializer = credential,
-                ApplicationName = ConfigurationManager.AppSettings["ApplicationName"],
-            });
+                Console.WriteLine("1) Getting Service Credentials...");
+                var credential = ServiceCredentialsProvider.GetServiceCredentials();
+                Console.WriteLine("A Service Credential was successfully created !");
 
-            var spreadsheetId = ConfigurationManager.AppSettings["SpreadSheetId"];
-            var range = ConfigurationManager.AppSettings["Range"];
-            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+                var service = new SheetsService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ConfigurationManager.AppSettings["ApplicationName"],
+                });
 
-            var response = request.Execute();
-            var values = response.Values;
-            var json = JsonConvert.SerializeObject(new Columns() { Rows = values });
+                var spreadsheetId = ConfigurationManager.AppSettings["SpreadSheetId"];
+                var range = ConfigurationManager.AppSettings["Range"];
+                SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
 
-            XmlDocument doc = JsonConvert.DeserializeXmlNode(json, "Columns");
-            doc.Save(ConfigurationManager.AppSettings["OutputXmlFile"]);            
+                Console.WriteLine("2) Getting SpreadSheet from Google Drive...");
+                var response = request.Execute();
+                var values = response.Values;
+                Console.WriteLine("SpreadSheet values are received sucessfully!");
+
+                Console.WriteLine("3) Serializing information received from SpreadSheet....");
+                var json = JsonConvert.SerializeObject(new Columns() { Rows = values });
+                XmlDocument doc = JsonConvert.DeserializeXmlNode(json, "Columns");
+                doc.Save(ConfigurationManager.AppSettings["OutputXmlFile"]);
+                Console.WriteLine("4) We finished!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Error Ocurred: {0}", ex.Message));
+            }
         }
     }
 }
